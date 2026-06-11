d6a4fecfff1d8a929efef9029fe2775cb9b106c5
Zack-Dugue <zdugue@smu.edu>
2026-06-11 13:17:14 -0500

    Fixed the way that the distances are computed

--- find_max_exploration.py
+++ find_max_exploration.py
@@ -221,5 +221,5 @@ if __name__ == "__main__":
         n_trials=100,
         num_samples=3,
         gamma=2,
-        write_dir="outputs/test_run_4",
+        write_dir="outputs/test_run_5",
     )
